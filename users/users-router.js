1187d06bb153dd037ff9de87977baa0e9c583fc8
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-11-02 14:34:04 -0800

    added logout endpoint from guided

--- users/users-router.js
+++ users/users-router.js
@@ -10,12 +10,29 @@ router.post("/login", async (req, res)=>{
     }
 });
 
-router.post("/signup", async (req, res)=>{
+router.post("/register", async (req, res)=>{
     try {
 
     } catch(err) {
         next(err)
     }
 });
+
+router.get("/logout", async (req, res, next) => {
+	try {
+		// this will delete the session in the database and try to expire the cookie,
+		// though it's ultimately up to the client if they delete the cookie or not.
+		// but it becomes useless to them once the session is deleted server-side.
+		req.session.destroy((err) => {
+			if (err) {
+				next(err)
+			} else {
+				res.status(204).end()
+			}
+		})
+	} catch (err) {
+		next(err)
+	}
+})
 
 module.exports = router;
